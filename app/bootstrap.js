5e830940fa96d3eb1c19da45047095e427f883ae
dovyden <dovyden@yandex.ru>
2017-10-22 14:42:00 +0300

    added mix method to instance (result) of `b_.lock`

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import store from './utils/store';
 
+// we should extend b_ before it will be used
+import 'utils/b-extends';
+
 import './components/Page/Page';
 import App from './components/App/App';
 
