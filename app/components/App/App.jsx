731a89a8167761e26a9a61f75313b1e26da4fd93
Smolya <mr.smolya@gmail.com>
2017-10-24 12:18:10 +0300

    app change

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
 import layout from '../Layout/Layout';
 import isAuth from '../../containers/Auth/isAuth';
 import GetToken from '../../containers/App/GetToken';
-import './App.css';
 
 export default function App() {
     return (
         <BrowserRouter>
             <Switch>
-                {/* Instead RRv3 hooks */}
                 <Route exact path={'/'} component={isAuth(layout)} />
-                <Route exact path={'/:auth?/:key?'} component={GetToken} />
+                <Route exact path={'/auth/:key?'} component={GetToken} />
             </Switch>
         </BrowserRouter>
     );
