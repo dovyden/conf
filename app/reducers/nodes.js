507f06a209eaf0f41d0977824d64a2c07b0986df
Gorizory <sobenin.g@gmail.com>
2017-10-25 13:02:21 +0300

    nodes.js fixed

--- app/reducers/nodes.js
+++ app/reducers/nodes.js
@@ -1,62 +1,60 @@
 export default function nodesReducer() {
-    return () => {
-        return {
-            "5@test.knevod.com": {
-                "TYPE": "NODE",
-                "name1": "\"Ингипро-тест-невод\"",
-                "removed": 0,
-                "hier/children": "[\"h@test.knevod.com\",\"2V@test.knevod.com\",\"41@test.knevod.com\",\"6V@test.knevod.com\"]",
-                "hier/parent": 0,
-                "doc/versions": "[]",
-                "sec/group": "[]",
-                "sec/mode": 511,
-                "sec/owner": "5@test.knevod.com"
-            },
-            "41@test.knevod.com": {
-                "TYPE": "NODE",
-                "name1": "dovyden",
-                "journalId": "3_@test.knevod.com",
-                "doc/versions": "[{\"a\":\"9@test.knevod.com\",\"j\":\"40@test.knevod.com\",\"ver\":1505375948588}]",
-                "hier/children": "[\"4G@test.knevod.com\",\"4L@test.knevod.com\"]",
-                "hier/parent": "5@test.knevod.com",
-                "sec/group": "[\"9@test.knevod.com\"]",
-                "sec/mode": 511,
-                "sec/owner": "41@test.knevod.com"
-            },
-            "6V@test.knevod.com": {
-                "TYPE": "NODE",
-                "name1": "klevzoff",
-                "journalId": "6T@test.knevod.com",
-                "doc/versions": "[{\"a\":\"7@test.knevod.com\",\"j\":\"6U@test.knevod.com\",\"ver\":1506442200688}]",
-                "hier/children": "[\"6-@test.knevod.com\",\"78@test.knevod.com\",\"7d@test.knevod.com\",\"cu@test.knevod.com\",\"cz@test.knevod.com\"]",
-                "hier/parent": "5@test.knevod.com",
-                "sec/group": "[\"7@test.knevod.com\"]",
-                "sec/mode": 511,
-                "sec/owner": "6V@test.knevod.com"
-            },
-            "h@test.knevod.com": {
-                "TYPE": "NODE",
-                "name1": "ixax",
-                "journalId": "f@test.knevod.com",
-                "removed": 0,
-                "doc/versions": "[{\"a\":\"6@test.knevod.com\",\"j\":\"g@test.knevod.com\",\"ver\":1502721004006}]",
-                "hier/children": "[\"k@test.knevod.com\",\"W@test.knevod.com\",\"1A@test.knevod.com\",\"1L@test.knevod.com\",\"2e@test.knevod.com\",\"2n@test.knevod.com\",\"31@test.knevod.com\",\"3n@test.knevod.com\",\"3S@test.knevod.com\",\"45@test.knevod.com\",\"4j@test.knevod.com\",\"57@test.knevod.com\",\"5B@test.knevod.com\",\"5F@test.knevod.com\",\"5N@test.knevod.com\",\"6C@test.knevod.com\",\"8b@test.knevod.com\",\"8l@test.knevod.com\",\"ai@test.knevod.com\",\"ap@test.knevod.com\",\"aE@test.knevod.com\",\"c5@test.knevod.com\",\"ci@test.knevod.com\",\"ry@test.knevod.com\",\"sU@test.knevod.com\",\"th@test.knevod.com\",\"tk@test.knevod.com\",\"tE@test.knevod.com\",\"uo@test.knevod.com\"]",
-                "hier/parent": "5@test.knevod.com",
-                "sec/group": "[\"6@test.knevod.com\"]",
-                "sec/mode": 511,
-                "sec/owner": "h@test.knevod.com"
-            },
-            "2V@test.knevod.com": {
-                "TYPE": "NODE",
-                "name1": "bobrof",
-                "journalId": "2T@test.knevod.com",
-                "doc/versions": "[{\"a\":\"d@test.knevod.com\",\"j\":\"2U@test.knevod.com\",\"ver\":1505056369858}]",
-                "hier/children": "[\"2-@test.knevod.com\",\"53@test.knevod.com\"]",
-                "hier/parent": "5@test.knevod.com",
-                "sec/group": "[\"d@test.knevod.com\"]",
-                "sec/mode": 511,
-                "sec/owner": "2V@test.knevod.com"
-            }
-        };
+    return {
+        "5@test.knevod.com": {
+            "TYPE": "NODE",
+            "name1": "\"Ингипро-тест-невод\"",
+            "removed": 0,
+            "hier/children": "[\"h@test.knevod.com\",\"2V@test.knevod.com\",\"41@test.knevod.com\",\"6V@test.knevod.com\"]",
+            "hier/parent": 0,
+            "doc/versions": "[]",
+            "sec/group": "[]",
+            "sec/mode": 511,
+            "sec/owner": "5@test.knevod.com"
+        },
+        "41@test.knevod.com": {
+            "TYPE": "NODE",
+            "name1": "dovyden",
+            "journalId": "3_@test.knevod.com",
+            "doc/versions": "[{\"a\":\"9@test.knevod.com\",\"j\":\"40@test.knevod.com\",\"ver\":1505375948588}]",
+            "hier/children": "[\"4G@test.knevod.com\",\"4L@test.knevod.com\"]",
+            "hier/parent": "5@test.knevod.com",
+            "sec/group": "[\"9@test.knevod.com\"]",
+            "sec/mode": 511,
+            "sec/owner": "41@test.knevod.com"
+        },
+        "6V@test.knevod.com": {
+            "TYPE": "NODE",
+            "name1": "klevzoff",
+            "journalId": "6T@test.knevod.com",
+            "doc/versions": "[{\"a\":\"7@test.knevod.com\",\"j\":\"6U@test.knevod.com\",\"ver\":1506442200688}]",
+            "hier/children": "[\"6-@test.knevod.com\",\"78@test.knevod.com\",\"7d@test.knevod.com\",\"cu@test.knevod.com\",\"cz@test.knevod.com\"]",
+            "hier/parent": "5@test.knevod.com",
+            "sec/group": "[\"7@test.knevod.com\"]",
+            "sec/mode": 511,
+            "sec/owner": "6V@test.knevod.com"
+        },
+        "h@test.knevod.com": {
+            "TYPE": "NODE",
+            "name1": "ixax",
+            "journalId": "f@test.knevod.com",
+            "removed": 0,
+            "doc/versions": "[{\"a\":\"6@test.knevod.com\",\"j\":\"g@test.knevod.com\",\"ver\":1502721004006}]",
+            "hier/children": "[\"k@test.knevod.com\",\"W@test.knevod.com\",\"1A@test.knevod.com\",\"1L@test.knevod.com\",\"2e@test.knevod.com\",\"2n@test.knevod.com\",\"31@test.knevod.com\",\"3n@test.knevod.com\",\"3S@test.knevod.com\",\"45@test.knevod.com\",\"4j@test.knevod.com\",\"57@test.knevod.com\",\"5B@test.knevod.com\",\"5F@test.knevod.com\",\"5N@test.knevod.com\",\"6C@test.knevod.com\",\"8b@test.knevod.com\",\"8l@test.knevod.com\",\"ai@test.knevod.com\",\"ap@test.knevod.com\",\"aE@test.knevod.com\",\"c5@test.knevod.com\",\"ci@test.knevod.com\",\"ry@test.knevod.com\",\"sU@test.knevod.com\",\"th@test.knevod.com\",\"tk@test.knevod.com\",\"tE@test.knevod.com\",\"uo@test.knevod.com\"]",
+            "hier/parent": "5@test.knevod.com",
+            "sec/group": "[\"6@test.knevod.com\"]",
+            "sec/mode": 511,
+            "sec/owner": "h@test.knevod.com"
+        },
+        "2V@test.knevod.com": {
+            "TYPE": "NODE",
+            "name1": "bobrof",
+            "journalId": "2T@test.knevod.com",
+            "doc/versions": "[{\"a\":\"d@test.knevod.com\",\"j\":\"2U@test.knevod.com\",\"ver\":1505056369858}]",
+            "hier/children": "[\"2-@test.knevod.com\",\"53@test.knevod.com\"]",
+            "hier/parent": "5@test.knevod.com",
+            "sec/group": "[\"d@test.knevod.com\"]",
+            "sec/mode": 511,
+            "sec/owner": "2V@test.knevod.com"
+        }
     };
 }
