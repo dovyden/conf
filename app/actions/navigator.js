7c8a44be2e353dc1788603de4fa43147fe0aa5e9
Gorizory <sobenin.g@gmail.com>
2017-11-04 17:50:50 +0300

    Fixed deserialization, added root

--- app/actions/navigator.js
+++ app/actions/navigator.js
@@ -2,6 +2,29 @@ import {NODE_DOWNLOADED} from '../constants/navigator';
 
 import {fetchApi} from '../utils/fetch';
 
+const deserialize = (json) => {
+    const nodes = json.nodes;
+    const result = {};
+    for (const key1 in nodes) {
+        if (!nodes.hasOwnProperty(key1)) {
+            continue;
+        }
+        const node = {};
+        for (const key2 in nodes[key1]) {
+            if (!nodes[key1].hasOwnProperty(key2)) {
+                continue;
+            }
+            try {
+                node[key2] = JSON.parse(nodes[key1][key2]);
+            } catch (SyntaxError) {
+                node[key2] = nodes[key1][key2];
+            }
+        }
+        result[key1] = node;
+    }
+    return result;
+};
+
 export const fetchNodes = (params) => {
     return (dispatch) => {
         const {query, attrs} = params;
@@ -10,11 +33,14 @@ export const fetchNodes = (params) => {
             attrs
         }}).then(res => {
             return res.json();
-        }).then(json => {
-            dispatch({
-                type: NODE_DOWNLOADED,
-                payload: json
-            });
-        });
+        }).then(
+            (json) => {
+                const data = deserialize(json);
+                dispatch({
+                    type: NODE_DOWNLOADED,
+                    payload: data
+                });
+            }
+        );
     };
 };
--- app/reducers/navigator.js
+++ app/reducers/navigator.js
@@ -1,19 +1,27 @@
 import {NODE_DOWNLOADED} from '../constants/navigator';
 
 const initialState = {
-    nodes: {},
-    root: []
+    nodes: {}
 };
 
 export default function navigatorReducer(state = initialState, {type, payload}) {
     switch (type) {
         case NODE_DOWNLOADED: {
+            let root = [];
+            if (state.root === undefined) {
+                for (const key in payload) {
+                    if (payload.hasOwnProperty(key)) {
+                        root.push(key);
+                    }
+                }
+            } else {
+                root = state.root;
+            }
             return {
-                root: state.root,
-                nodes: Object.assign(state.nodes, payload.nodes)
+                root,
+                nodes: Object.assign(state.nodes, payload)
             };
         }
     }
-
     return state;
 }
