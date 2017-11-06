5ae3b198a8fa0bdfd0c0de7a72caae7fcef37ecf
podlovchenko <podlovchenko.vera@gmail.com>
2017-11-06 14:07:12 +0300

    верстка

--- app/components/Toolbar/Toolbar.jsx
+++ app/components/Toolbar/Toolbar.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
+import DynamicReport from '../../containers/DynamicReport/DynamicReport';
 
-export default function Toolbar() {
-    return (
-        <div className="toolbar">
-            Toolbar
-        </div>
-    );
+import './css/Toolbar.css';
+
+class Toolbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            openTab: {}
+        };
+    }
+
+    render() {
+        return (
+            <div className="toolbar">
+                <div
+                    className={this.state.openTab.dynamicReport
+                        ? 'toolbar__item toolbar__item_active'
+                        : 'toolbar__item'}
+                    onClick={() => {
+                        const newState = this.state;
+                        newState.openTab.dynamicReport
+                            ? delete newState.openTab.dynamicReport
+                            : newState.openTab.dynamicReport = true;
+                        this.setState(newState);
+                    }}
+                >
+                    <div
+                        className={this.state.openTab.dynamicReport
+                            ? 'toolbar__icon toolbar__icon_active'
+                            : 'toolbar__icon'}
+                    >
+                        DR
+                    </div>
+                </div>
+                <div className="toolbar__content">
+                    {this.state.openTab.dynamicReport && <DynamicReport/>}
+                </div>
+            </div>
+        );
+    }
 }
+
+export default Toolbar;
