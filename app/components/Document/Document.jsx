f99ce5e552d9044b8b328c83e674d6aefc3f355f
Seva <seva213415@inbox.ru>
2017-11-06 17:19:33 +0300

    simply zoom works

--- app/components/Document/Document.jsx
+++ app/components/Document/Document.jsx
@@ -4,8 +4,7 @@ import {Path, paper, Point} from 'paper';
 
 export default class DocumentView extends Component {
     onMouseDown(event) {
-        if (!event.ctrlKey) {
-            this.flagMouseDownForPaper = false; // по логике не нужно - для читаемости
+        if (!event.originalEvent.ctrlKey) {
             return;
         }
 
@@ -13,38 +12,38 @@ export default class DocumentView extends Component {
         // constrainDuringPan
         this.viewer.panVertical = false;
         this.viewer.panHorizontal = false;
-        this.viewer.gestureSettingsMouse.scrollToZoom = false;
-        const x = event.pageX - this.left;
-        const y = event.pageY - this.top;
+        this.viewer.zoomPerScroll = 1; // forbid zoom
+        const x = event.position.x;
+        const y = event.position.y;
+        // const x = event.pageX - this.left;
+        // const y = event.pageY - this.top;
         this.marks[this.countOfMarks] = {
             path: new Path({
-                segments: [x, y],
+                segments: [new Point(x, y)],
                 strokeColor: '#ff0000',
                 strokeWidth: 5,
             }),
             bound: null,
-            zoom: null
+            zoom: null,
+            currentZoom: null
         };
     }
 
-    onMouseDrag(event, delta) {
+    onMouseDrag(event, delta, x, y) {
         if (this.flagMouseDownForPaper) {
             if (!event.ctrlKey) {
                 this.onMouseUp();
             }
-            const x = event.pageX - this.left;
-            const y = event.pageY - this.top;
-            this.marks[this.countOfMarks].path.add(x, y);
+            this.marks[this.countOfMarks].path.add(new Point(x, y));
         } else if (this.countOfMarks) {
             /**
              * TODO: when there is no pan because of the fact that the drawing gets on the screen,
              * the marks should not move
              */
-            // const newZoom = this.viewer.viewport.getZoom(true) /  this.marks[0].zoom;
-            // console.log(newZoom);
-            console.log(this.viewer.viewport.getZoom(true));
+            const currentZoom = this.viewer.viewport.getZoom(true);
             this.marks.forEach((item) => {
-                const deltaPath = new Point(delta.x, delta.y);
+                const newZoom = currentZoom / item.currentZoom;
+                const deltaPath = new Point(delta.x / newZoom, delta.y / newZoom);
                 item.path.translate(deltaPath);
             });
         }
@@ -54,12 +53,15 @@ export default class DocumentView extends Component {
         if (this.flagMouseDownForPaper) {
             this.marks[this.countOfMarks].bound = this.viewer.viewport.getBounds(true);
             this.marks[this.countOfMarks].zoom = this.viewer.viewport.getZoom(true);
+            this.marks[this.countOfMarks].currentZoom = this.viewer.viewport.getZoom(true);
             this.countOfMarks++;
         }
         this.flagMouseDownForPaper = false;
+
+        event.preventDefaultAction = false;
         this.viewer.panVertical = true;
         this.viewer.panHorizontal = true;
-        this.viewer.gestureSettingsMouse.scrollToZoom = true;
+        this.viewer.zoomPerScroll = 0.75; // allow zoom
     }
 
     componentDidMount() {
@@ -76,15 +78,17 @@ export default class DocumentView extends Component {
         this.flagMouseDownForPaper = false;
 
         const idDocument = this.props.idDocument.toString();
+        // TODO: slow raise events to faster redraw marks
         this.viewer = new OpenSeadragon.Viewer({
             id: idDocument,
             showNavigationControl: false,
             gestureSettingsMouse: {clickToZoom: false},
             showNavigator: false,
-            zoomPerScroll: 0.5,
+            zoomPerScroll: 0.75,
             debugMode: false,
             visibilityRatio: 0.9,
             minZoomLevel: 1,
+            maxZoomLevel: 6,
             constrainDuringPan: true,
             tileSources: 'https://test.knevod.com/static/tile/_test/4H/4H@test.knevod.com/330/0/tiles.dzi'
         });
@@ -92,29 +96,38 @@ export default class DocumentView extends Component {
         this.viewer.addHandler('open', () => {
             const oldBounds = this.viewer.viewport.getBounds(true);
             const newBounds = new OpenSeadragon.Rect(0, 0, 1, oldBounds.height / oldBounds.width);
-            this.viewer.viewport.fitBounds(newBounds, true); // нужное место на рисунке
-            this.viewer.viewport.zoomTo(2); // нужный зум
+            this.viewer.viewport.fitBounds(newBounds, true);
+            this.viewer.viewport.zoomTo(1, true);
+            // TODO: method who redraw marks with newBounds and zoom
         });
 
-        this.viewer.addHandler('canvas-scroll', (event) => {
+        this.viewer.addHandler('zoom', (event) => {
+            if (!this.countOfMarks || this.flagMouseDownForPaper) {
+                return;
+            }
 
-            // if (this.marks[0].path === undefined || this.flagMouseDownForPaper) {
-            //     return;
-            // }
-            //
-            // this.marks.forEach((item) => {
-            //     const newZoom = this.viewer.viewport.getZoom(true) / item.zoom;
-            //     item.path.view.zoom = newZoom;
-            //     // item.path.scale(newZoom, new Point(item.path.handleBounds.center.x, item.path.handleBounds.center.y));
-            // });
+            const currentZoom = event.zoom;
+            if (currentZoom >= 6 || currentZoom <= 1) {
+                return;
+            }
+
+            // const newBounds = this.viewer.viewport.deltaPixelsFromPoints(this.viewer.viewport.getBounds(true));
+            // const delta = new Point(newBounds.x - this.prevBounds.x, newBounds.y - this.prevBounds.y);
+            this.marks.forEach((item) => {
+                const newZoom = currentZoom / item.currentZoom;
+                // delta.x /= newZoom;
+                // delta.y /= newZoom;
+                // item.path.translate(new Point(10, 10));
+                item.currentZoom = currentZoom;
+                item.path.scale(newZoom);
+            });
         });
 
         this.viewer.addHandler('canvas-press', (event) => {
-            // event.preventDefaultAction = true
-            this.onMouseDown(event.originalEvent);
+            this.onMouseDown(event);
         });
         this.viewer.addHandler('canvas-drag', (event) => {
-            this.onMouseDrag(event.originalEvent, event.delta);
+            this.onMouseDrag(event.originalEvent, event.delta, event.position.x, event.position.y);
 
         });
         this.viewer.addHandler('canvas-exit', (event) => {
