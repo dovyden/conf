2827e5dfa24ed305633f987e6df584d50e4f09d8
Smolya <mr.smolya@gmail.com>
2017-11-08 17:45:26 +0300

    update spinner-component edit b.mix using

--- app/components/Spinner/Spinner.jsx
+++ app/components/Spinner/Spinner.jsx
@@ -9,11 +9,10 @@ const b = b_.lock('spinner');
 // Example call of Spinner-component with mix: <Spinner className="toolbar__spinner" />
 // In result we'll give className: 'spinner toolbar__spinner'
 export default function Spinner({position, className}) {
-    // We call the spinner-component either with a `position` prop or with a `className` prop. No other way
-    const bemClass = className ? b.mix(className) : b({[`position_${position}`]: true});
-
+    // We call the spinner-component either with a `position` prop or with a `className` prop or both. No other way.
+    // In the future, there may be other modifiers
     return (
-        <div className={bemClass}
+        <div className={b.mix(className, {[`position`]: position})}
         />
     );
 }
