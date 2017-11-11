e6ad2b956da1fd702ff38054dd3b8064e67989aa
Gorizory <sobenin.g@gmail.com>
2017-11-11 21:32:38 +0300

    Markup (fixes)

--- app/components/Navigator/Node.jsx
+++ app/components/Navigator/Node.jsx
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import b_ from 'b_';
 
-import './Navigator.css';
+import './Node.css';
 
-const b = b_.lock('nodes-list');
+const b = b_.lock('node');
 
-export default function Toolbar(props) {
-    const {node} = props;
-    return [
-        <td key={'name'} className={b('text', {'node-attrs': true})}>
-            {node.name1}
-        </td>,
-        <td key={'owner'} className={b('text', {'node-attrs': true})}>
-            {node['sec/owner']};
-        </td>
-    ];
+class Node extends Component {
+    constructor(props) {
+        super(props);
+        this.navigateTo = this.navigateTo.bind(this);
+    }
+
+    render() {
+        const {node} = this.props;
+        return (
+            <tr
+                className={b()}
+                onClick={this.navigateTo}
+            >
+                <td key="name" className={b('text', {'node-attrs': true})}>
+                    {node.name1}
+                </td>
+                <td key="owner" className={b('text', {'node-attrs': true})}>
+                    {node['sec/owner']};
+                </td>
+            </tr>
+        );
+    }
+
+    navigateTo() {
+        this.props.navigateTo({
+            type: 'node',
+            nodeId: this.props.nodeId
+        });
+    }
 }
 
-Toolbar.propTypes = {
+Node.propTypes = {
     node: PropTypes.object.isRequired,
+    nodeId: PropTypes.string.isRequired,
+    navigateTo: PropTypes.func.isRequired
 };
+
+export default Node;
